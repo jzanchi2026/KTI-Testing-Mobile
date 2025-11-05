734371092e78167c3ac780ec93a326e6f55f1f6d
RomanNoodles00 <147829994+RomanNoodles00@users.noreply.github.com>
2025-11-05 09:06:20 -0500

    fixed search

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,60 +10,60 @@ namespace MauiApp2
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
-
-        private static List<Tool> toollist = new List<Tool>();
+        private static List<Tool> toollist = new();
 
         public MainPage()
         {
             InitializeComponent();
             _ = grabTools();
-
         }
+
         private async Task grabTools()
         {
             await ToolRepository.InitializeToolsAsync();
             List<Tool> tools = ToolRepository.GetTools();
 
             foreach (Tool i in tools)
-            {
                 addItem(i);
-            }
-
         }
+
         public void addItem(Tool tool)
         {
             toollist.Add(tool);
 
             var myStyle = new Style<Button>(
-
-            (Button.HeightRequestProperty, 120),
-            (Button.MaximumWidthRequestProperty, 430),
-            (Button.TextColorProperty, Colors.Black),
-            (Button.BackgroundColorProperty, Colors.Beige),
-            (Button.FontSizeProperty, 28)
+                (Button.HeightRequestProperty, 120),
+                (Button.MaximumWidthRequestProperty, 430),
+                (Button.TextColorProperty, Colors.Black),
+                (Button.BackgroundColorProperty, Colors.Beige),
+                (Button.FontSizeProperty, 28)
             );
 
-            Button button = new Button { Text = tool.Name, Style = myStyle };
-            button.Margin = new Thickness(15, 15, 15, 0);
+            var button = new Button
+            {
+                Text = tool.Name,
+                Style = myStyle,
+                Margin = new Thickness(15, 15, 15, 0),
+                BindingContext = tool // 👈 attach the whole tool object
+            };
 
             button.Clicked += async (s, e) =>
             {
-                await Shell.Current.GoToAsync($"{nameof(ToolInfo)}?Id={tool.Id}");
+                var btn = (Button)s;
+                var selectedTool = (Tool)btn.BindingContext; // 👈 retrieve attached tool
+                await Shell.Current.GoToAsync($"{nameof(ToolInfo)}?Id={selectedTool.Id}");
             };
-
-
 
             listBox.Children.Add(button);
         }
 
         private async void toolList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            if (toolList.SelectedItem != null)
+            if (e.SelectedItem is Tool selectedTool)
             {
-                await Shell.Current.GoToAsync($"{nameof(CartPage)}?Id={((Tools)toolList.SelectedItem).toolId}");
+                await Shell.Current.GoToAsync($"{nameof(ToolInfo)}?Id={selectedTool.Id}");
             }
-
-
+            toolList.SelectedItem = null;
         }
 
         private void toolList_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -76,12 +76,12 @@ namespace MauiApp2
             var tools = new ObservableCollection<Tool>(ToolRepository.GetTools());
             toolList.ItemsSource = tools;
         }
+
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
-            var tools = new ObservableCollection<Tool>(ToolRepository.SearchTools(((SearchBar)sender).Text));
+            var searchBar = (SearchBar)sender;
+            var tools = new ObservableCollection<Tool>(ToolRepository.SearchTools(searchBar.Text));
             toolList.ItemsSource = tools;
         }
     }
-    //test
 }
